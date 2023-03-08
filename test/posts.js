a032c50e9074486a2e9aca33f9da61732d5a17ef
sptalaxin <sptalaxin@bk.ru>
2023-03-09 00:32:04 +0300

    added negative tests

--- test/posts.js
+++ test/posts.js
@@ -13,19 +13,38 @@ describe('User Posts', () => {
        userID = await createRandomUser()
     });
 
+    describe('Positive Tests', () => {
         it('/posts', async () => {
             const data = {
                 user_id: userID,
                 title: "smth new with this title",
                 body: "whats up"
             }
-                    
+                        
             const postsRes = await request
                 .post('posts')
                 .set('Authorization', `Bearer ${token}`)
                 .send(data)
-
+    
                 expect(postsRes.body.body).to.eql('whats up')
                 expect(postsRes.body.user_id).to.eql(userID)
-                });
         });
+    });
+
+    describe('Negative Tests', () => {
+        it.only('401 Authentication Failed', async () => {
+            const data = {
+                user_id: userID,
+                title: "smth new with this title",
+                body: "whats up"
+            }
+                        
+            const postsRes = await request
+                .post('posts')
+                .send(data)
+
+                expect(postsRes.statusCode).to.equal(401)
+    
+        });
+    });
+});
