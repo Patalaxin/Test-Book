cd8e0a3a52aa7b951e04c7d6924abbd72f9f0ff8
sptalaxin <sptalaxin@bk.ru>
2023-03-09 00:09:55 +0300

    fixex issue with import

--- test/posts.js
+++ test/posts.js
@@ -1,6 +1,7 @@
 import supertest from "supertest";
 import { expect } from "chai";
-import { createRandomUser } from "/Users/spata/OneDrive/Desktop/bookstore-master/helper/user_helper.js";
+import { createRandomUser } from "../helper/user_helper.js";
+
 
 const request = supertest('https://gorest.co.in/public/v2/')
 const token = '83913acf66c386030b85a03d70c68b887aae619b1938b3079f91374b3147d386'
@@ -8,8 +9,8 @@ const token = '83913acf66c386030b85a03d70c68b887aae619b1938b3079f91374b3147d386'
 describe('User Posts', () => {
     let userID;
 
-    before(async() => {
-       userID = await createRandomUser();
+    before( async () => {
+       userID = await createRandomUser()
     });
 
         it('/posts', async () => {
